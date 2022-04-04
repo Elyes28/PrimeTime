dceec86970d0cbe59659dbff20c00df5bf3270be
Elyes28 <49162012+Elyes28@users.noreply.github.com>
2022-04-04 01:27:08 +0100

    Launchstream fix

--- unice_theme/pages/streams/launchStream.js
+++ unice_theme/pages/streams/launchStream.js
@@ -1,18 +1,24 @@
-import React from 'react';
+
 import {Container,Row} from 'reactstrap'
-import CommonLayout from "../../../containers/common/common-layout"
-import DetailSecion from '../../../containers/blog/card/detail-wrapper'
+import JoinScreen from './iframe.js'
+import CommonLayout from '../../containers/common/common-layout'
+import React, { useState, Fragment, useEffect } from 'react';
+
+
 
 const DetailNoSidebar = () => (
+    
     <CommonLayout pathList={['blog', 'blog details', 'no sidebar']} pathTitle="BLOG WITH no-sidebar">
         <section className="agency blog-sec blog-sidebar single_blog_item">
             <Container>
                 <Row>
-                    <DetailSecion className="col-12" />
+                    <JoinScreen></JoinScreen>
+                
                 </Row>
             </Container>
         </section>
     </CommonLayout>
 )
+
 
 export default DetailNoSidebar;
