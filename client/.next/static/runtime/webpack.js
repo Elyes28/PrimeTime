1bb21756e4f3f97c57e866f3f1626690a7db1887
Elyes28 <49162012+Elyes28@users.noreply.github.com>
2022-04-27 01:08:41 +0100

    push

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "298636df826cf1e68ff4";
+/******/ 	var hotCurrentHash = "505dc63e608a20c5844b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
